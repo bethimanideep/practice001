f08499eb9f66c3b7b78fb1dec262a183989cf813
Manideep-0164 <peddaboinimanideep03@gmail.com>
2022-11-02 18:24:15 +0530

    code fixed

--- primepractice.js
+++ primepractice.js
@@ -1,1 +1,19 @@
 //prime code
+function checkprime(num)
+{
+let count=0;
+for(let i=1; i<=num; i++)
+{
+if(num%i==0)
+{
+count++;
+}
+}
+if(count==2)
+{
+return true;
+} return false;
+}
+let ans=checkprime(17)
+console.log(ans);
+
